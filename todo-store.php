73f9012ef2b6e0477b4c64411f3a27253bf40fdb
rshal <r.shalihin143@gmail.com>
2024-04-19 18:08:51 +0600

    minor update

--- todo-store.php
+++ todo-store.php
@@ -17,13 +17,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-/**
- * Registers the block using the metadata loaded from the `block.json` file.
- * Behind the scenes, it registers also all assets so they can be enqueued
- * through the block editor in the corresponding context.
- *
- * @see https://developer.wordpress.org/reference/functions/register_block_type/
- */
 function create_block_todo_store_block_init() {
 	$asset_file = include plugin_dir_path( __FILE__ ) . 'build/index.asset.php';
 	wp_enqueue_script( 'block-todo-plugin-script', plugins_url( 'build/index.js', __FILE__ ), $asset_file['dependencies'], $asset_file['version'] );
